fde03c1b2d184da3b5e5b2bef95afdb3509ab5e9
Erik Fleckstein <erik@tinkerforge.com>
2020-08-25 15:53:06 +0200

    uc: Fix whitespace

--- software/examples/uc/example_output.c
+++ software/examples/uc/example_output.c
@@ -1,14 +1,9 @@
-
-
 #include "bindings/hal_common.h"
 #include "bindings/bricklet_io4_v2.h"
 
 #define UID "XYZ" // Change XYZ to the UID of your IO-4 Bricklet 2.0
 
 void check(int rc, const char* msg);
-
-
-
 
 TF_IO4V2 io;
 
